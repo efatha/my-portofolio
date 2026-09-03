c1f7d03b4e312ff5c5dce52b4b6d356b00e408a7
efatha <efathabyamungu4@gmail.com>
2026-09-03 23:35:08 +0300

    Updated the contact autowire

--- backend/src/main/java/com/efatha/backend/contact/ContactMessageService.java
+++ backend/src/main/java/com/efatha/backend/contact/ContactMessageService.java
@@ -1,5 +1,6 @@
 package com.efatha.backend.contact;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +13,7 @@ public class ContactMessageService {
     private final ContactMessageRepository repository;
     private final Clock clock;
 
+    @Autowired
     public ContactMessageService(ContactMessageRepository repository) {
         this(repository, Clock.systemUTC());
     }
