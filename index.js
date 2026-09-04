e397962b4bbe2d479119a22f395837ff85e61871
efatha <efathabyamungu4@gmail.com>
2026-09-04 15:00:35 +0300

    Fixed the fetch to the right databse

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
       submitButton.disabled = true;
 
       try {
-        const response = await fetch("https://my-portofolio-backend.onrender.com/api/contact", {
+        const response = await fetch("https://my-portofolio-j8da.onrender.com/api/contact", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(Object.fromEntries(new FormData(contactForm)))
